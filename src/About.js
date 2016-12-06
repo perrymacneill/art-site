36433d99e365c3f813f5684a7678f1f038ff41bd
Perry MacNeill <perry@perrymacneill.com>
2016-12-06 15:21:17 -0500

    removed localhost url

--- src/About.js
+++ src/About.js
@@ -7,7 +7,7 @@ class About extends Component {
     return (
       <div id="about-container" style={{height: '100%', position: 'relative'}}>
         <Card shadow={0} style={{width: '340px', margin: 'auto'}}>
-          <CardTitle style={{color: '#424242', height: '470px', background: 'url(http://localhost:3000/images/about.jpg) center / cover'}}>About</CardTitle>
+          <CardTitle style={{color: '#424242', height: '470px', background: 'url(/images/about.jpg) center / cover'}}>About</CardTitle>
           <CardText>
             Based in Ann Arbor, MI, Tyler Baker is an artist whose work conveys the cycles of life and death in their many forms using minimal compositions of industry, light and decay.
           </CardText>
